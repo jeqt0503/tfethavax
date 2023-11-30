fe417514893f26304d5e38842e5ccde347a16989
jeqt0503 <144416997+jeqt0503@users.noreply.github.com>
2023-11-30 12:08:53 +0800

    Update jdgtfavax.sol

--- jdgtfavax.sol
+++ jdgtfavax.sol
@@ -5,24 +5,33 @@ import "@openzeppelin/contracts@4.9.0/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts@4.9.0/access/Ownable.sol";
 
 contract jeToken is ERC20, Ownable {
-    constructor(uint256 iSupply) ERC20("ERC20", "ETH") {
-        _mint(msg.sender, iSupply);
+    uint256 private constant MINIMUM_BURN_AMOUNT = 15;
+
+    event TokensBurned(address indexed burner, uint256 amount);
+
+    constructor(address initialOwner, uint256 initialSupply) ERC20("jeToken", "JET") Ownable() {
+        _mint(initialOwner, initialSupply);
     }
 
-    function burn(uint256 amount) 
-    public {
-        require(amount > 15, "Must be greater than 15");
-        require(amount <= balanceOf(msg.sender), "Insufficient balance");
-        _burn(msg.sender, amount);
+    function burn(uint256 amount) external {
+        _burnLogic(msg.sender, amount);
     }
 
-    function mint(uint256 amount) 
-    public onlyOwner {
-        _mint(msg.sender, amount);
+
+    function mint(address account, uint256 amount) external onlyOwner {
+        _mint(account, amount);
     }
 
     function transfer(address to, uint256 amount) public override returns (bool) {
-        _transfer(_msgSender(), to, amount);    
+        _transfer(_msgSender(), to, amount);
         return true;
+    }
+
+    function _burnLogic(address account, uint256 amount) internal {
+        require(amount >= MINIMUM_BURN_AMOUNT, "Amount must be greater than or equal to 15");
+        require(amount <= balanceOf(account), "Insufficient balance");
+
+        _burn(account, amount);
+        emit TokensBurned(account, amount);
     }
 }
